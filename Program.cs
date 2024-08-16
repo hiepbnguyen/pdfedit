91837363f1fe4e5fe7cc9faf91bb44573a5dbb04
Hiep Nguyen <hiepbnguyen@outlook.com>
2024-08-16 00:15:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using iText.Forms.Xfa;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText;
+using iText.Forms;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String inputFileDir = @"C:\Users\hiepn\OneDrive\Desktop\personal-projects\pdfmaker\";
+String destFile = @"C:\Users\hiepn\OneDrive\Desktop\personal-projects\pdfmaker\new.pdf";
+
+PdfReader reader = new PdfReader(inputFileDir + "8615.pdf");
+PdfWriter writer = new PdfWriter(destFile);
+PdfDocument pdfDoc = new PdfDocument(reader, writer);
+XfaForm xfa = PdfAcroForm.GetAcroForm(pdfDoc, false).GetXfaForm();
+System.Xml.Linq.XDocument domDoc = xfa.GetDomDocument();
+Console.WriteLine(domDoc.ToString());
